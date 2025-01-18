70e81cde6f7f5ea31ced96131fb52a1144c6e37b
GueLaKais <koroyeldiores@gmail.com>
2025-01-18 21:22:02 +0100

    added new options to give jovian the basic functionality to get itself wether it has a enabled desktopsession or not

--- modules/steam/autostart.nix
+++ modules/steam/autostart.nix
@@ -1,4 +1,9 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   inherit (lib)
@@ -7,7 +12,7 @@ let
     mkMerge
     mkOption
     types
-  ;
+    ;
   cfg = config.jovian.steam;
 in
 {
@@ -32,16 +37,34 @@ in
         };
 
         desktopSession = mkOption {
-          type = with types ; nullOr str // {         
-            check = userProvidedDesktopSession:
-              lib.assertMsg (userProvidedDesktopSession != null -> (str.check userProvidedDesktopSession && lib.elem userProvidedDesktopSession config.services.displayManager.sessionData.sessionNames)) ''
-                  Desktop session '${userProvidedDesktopSession}' not found.
-                  Valid values for 'jovian.steam.desktopSession' are:
-                    ${lib.concatStringsSep "\n  " config.services.displayManager.sessionData.sessionNames}
-                  If you don't want a desktop session to switch to, set 'jovian.steam.desktopSession' to 'gamescope-wayland'.
-              '';
-          };
-          default = null;
+          type =
+            with types;
+            nullOr str
+            // {
+              check =
+                userProvidedDesktopSession:
+                lib.assertMsg
+                  (
+                    userProvidedDesktopSession != null
+                    -> (
+                      str.check userProvidedDesktopSession
+                      && lib.elem userProvidedDesktopSession config.services.displayManager.sessionData.sessionNames
+                    )
+                  )
+                  ''
+                    Desktop session '${userProvidedDesktopSession}' not found.
+                    Valid values for 'jovian.steam.desktopSession' are:
+                      ${lib.concatStringsSep "\n  " config.services.displayManager.sessionData.sessionNames}
+                    If you don't want a desktop session to switch to, set 'jovian.steam.desktopSession' to 'gamescope-wayland'.
+                  '';
+            };
+          default =
+            if config.services.desktopManager.plasma6.enable then
+              "plasma"
+            else if config.services.desktopManager.lomiri.enable then
+              "lomiri"
+            else
+              "gamescope-wayland";
           example = "plasma";
           description = ''
             The session to launch for Desktop Mode.
@@ -90,11 +113,14 @@ in
           ]
           # Add any globally defined well-known XKB_DEFAULT environment variables to the session
           # This is the closest wayland sessions have to generic keyboard configurations.
-          ++ (map (var:
-            (mkIf (config.environment.variables ? "${var}") {
-              "${var}" = mkDefault config.environment.variables."${var}";
-            })
-          ) [
+          ++ (map
+            (
+              var:
+              (mkIf (config.environment.variables ? "${var}") {
+                "${var}" = mkDefault config.environment.variables."${var}";
+              })
+            )
+            [
               "XKB_DEFAULT_LAYOUT"
               "XKB_DEFAULT_OPTIONS"
               "XKB_DEFAULT_MODEL"
